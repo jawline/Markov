404e9e6f086cab1acad499af00665498445fb4ea
Blake Loring <blake_l@parsed.uk>
2016-12-26 13:56:42 +0000

    More cleaning up

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,35 @@ use std::vec::Vec;
 use std::collections::VecDeque;
 use rand::distributions::{IndependentSample, Range};
 
-const LEADING_WORDS: usize = 2;
-
 type Prefix = VecDeque<String>;
 type State = HashMap<Prefix, Vec<String>>;
 
 pub struct Markov {
 	state: State,
-	prefix: Prefix
+	prefix: Prefix,
+	leading: usize
 }
 
 impl Markov {
-	pub fn new() -> Markov {
+	pub fn new(leading: usize) -> Markov {
+
 		let mut n = Markov {
 			state: State::new(),
-			prefix: Prefix::new()
+			prefix: Prefix::new(),
+			leading: leading
 		};
+
+		//Pad the initial prefix with rubbish
+		n.pad();
+
 		n
 	}
 
 	/**
+	 * Adds a word from the source text (Should be added in order)
+	 *
 	 * For some state, prefix and word
+	 * let LEADING_WORDS = self.leading
 	 *
 	 * If the prefix is larger than LEADING_WORDS then
 	 * add a transition entry from prefix -> word then
@@ -43,7 +51,7 @@ impl Markov {
 	 */
 	pub fn add(&mut self, word: &str) {
 		let p_size = self.prefix.len();
-		if p_size == LEADING_WORDS {
+		if p_size == self.leading {
 			self.state.entry(self.prefix.clone()).or_insert(Vec::new()).push(word.to_string());
 			self.prefix.pop_front();
 		}
@@ -60,6 +68,17 @@ impl Markov {
 		follows[between.ind_sample(&mut rng)].to_string()
 	}
 
+	/**
+	 * Generates the next word randomly
+	 * 
+	 * Given some prefix and state
+	 * 
+	 * let follow = all the words which follow the current prefix in state
+	 * let the next word equal a random word in follow
+	 * Finally remove the first element of the prefix and add the new word
+	 * to the end of the prefix. This acts to advance the prefix so that a word
+	 * which follows the selected will follow next time this method is called
+	 */
 	pub fn next(&mut self) -> String {
 		let nword = Markov::select(self.state.entry(self.prefix.clone()).or_insert(Vec::new()));
 
@@ -73,24 +92,29 @@ impl Markov {
 	}
 
 	fn pad(&mut self) {
-		for _ in 0..LEADING_WORDS {
+		for _ in 0..self.leading {
 			self.add("\n");
 		}
 	}
 }
 
-fn main() {
-	
-	let mut m = Markov::new();
+pub fn from_file(path: &str) -> Markov {
+	let mut m = Markov::new(1);
 
 	let mut data = "".to_string();
-	File::open("./sample").expect("Unable to open file").read_to_string(&mut data).expect("Unable to read string");
+
+	File::open(path).expect("Unable to open file").read_to_string(&mut data).expect("Unable to read string");
 
 	for d in data.split(" ") {
 		m.add(d);
 	}
 
 	m.finalize();
+	m
+}
+
+fn main() {
+	let mut m = from_file("./sample");
 
 	for _ in 0..100000000 {
 		print!("{} ", m.next());
